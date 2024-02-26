a98ff6ec95047450fe8fd43b8e22c04529d12f36
Valeria375 <146752405+Valeria375@users.noreply.github.com>
2024-02-26 13:43:24 +0300

    изм по первому ревью

--- src/index.js
+++ src/index.js
@@ -1,83 +1,72 @@
 import "./pages/index.css";
 import { openModal, closeModal } from "./scripts/modals";
-
 import { initialCards } from "./scripts/cards";
 import { createCard, removeCard, likeCard } from "./scripts/card";
 // @todo: Темплейт карточки
 // @todo: DOM узлы
 const cardsContainer = document.querySelector(".places__list");
-// @todo: Функция создания карточки
-
-const popupTypeImage = document.querySelector('.popup_type_image');
-const popupTypeImageClose = popupTypeImage.querySelector('.popup__close');
-const popupImage = popupTypeImage.querySelector('.popup__image');
-const popupCaption = popupTypeImage.querySelector('.popup__caption');
+const popupTypeImage = document.querySelector(".popup_type_image");
+const popupTypeImageClose = popupTypeImage.querySelector(".popup__close");
+const popupImage = popupTypeImage.querySelector(".popup__image");
+const popupCaption = popupTypeImage.querySelector(".popup__caption");
+const editProfileButton = document.querySelector(".profile__edit-button");
+const formEditProfile = document.querySelector(".popup_type_edit");
+const createNewCard = document.querySelector(".profile__add-button");
+const formAddCard = document.querySelector(".popup_type_new-card");
+const formProfile = formEditProfile.querySelector(".popup__form"); // Воспользуйтесь методом querySelector()
+const formCard = formAddCard.querySelector(".popup__form");
+// Находим поля формы в DOM
+const nameInput = formProfile.querySelector(".popup__input_type_name"); // Воспользуйтесь инструментом .querySelector()
+const jobInput = formProfile.querySelector(".popup__input_type_description"); // Воспользуйтесь инструментом .querySelector()
+const nameCardInput = formCard.querySelector(".popup__input_type_card-name"); // Воспользуйтесь инструментом .querySelector()
+const linkInput = formCard.querySelector(".popup__input_type_url"); // Воспользуйтесь инструментом .querySelector()
+const profileTitle = document.querySelector(".profile__title");
+const profileDesc = document.querySelector(".profile__description");
+const closeEditProfileButton = formEditProfile.querySelector(".popup__close");
 
 function openImage(imageSrc, descriptionText) {
   popupImage.src = imageSrc;
   popupImage.alt = descriptionText;
   popupCaption.textContent = descriptionText;
   openModal(popupTypeImage);
-};
-popupTypeImageClose.addEventListener('click', () => closeModal(popupTypeImage));
-
+}
+popupTypeImageClose.addEventListener("click", () => closeModal(popupTypeImage));
 function placeCard(card, container) {
   const cardItem = createCard(card, removeCard, likeCard, openImage);
   container.append(cardItem);
 }
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
 initialCards.forEach((card) => {
   placeCard(card, cardsContainer);
 });
-
-const editProfileButton = document.querySelector(".profile__edit-button");
-const formEditProfile = document.querySelector(".popup_type_edit");
 editProfileButton.addEventListener("click", function () {
-  nameInput.value ='';
-  jobInput.value ='';
+  nameInput.value;
+  jobInput.value;
   openModal(formEditProfile);
-  const popClose = formEditProfile.querySelector(".popup__close");
-  popClose.addEventListener("click", function () {
-    closeModal(formEditProfile);
-  });
 });
-
-const createNewCard = document.querySelector(".profile__add-button");
-const formAddCard = document.querySelector(".popup_type_new-card");
+closeEditProfileButton.addEventListener("click", function () {
+  closeModal(formEditProfile);
+});
 createNewCard.addEventListener("click", function () {
-  nameCardInput.value ='';
-  linkInput.value ='';
+  nameCardInput.value = "";
+  linkInput.value = "";
   openModal(formAddCard);
-  const popupClose = formAddCard.querySelector(".popup__close");
-  popupClose.addEventListener("click", function () {
-    closeModal(formAddCard);
-  });
+});
+popupTypeImageClose.addEventListener("click", function () {
+  closeModal(formAddCard);
 });
 //изменение форм в модальных окнах
 // Находим форму в DOM
-const formElement = formEditProfile.querySelector(".popup__form"); // Воспользуйтесь методом querySelector()
-const formEl = formAddCard.querySelector(".popup__form");
-// Находим поля формы в DOM
-const nameInput = formElement.querySelector(".popup__input_type_name"); // Воспользуйтесь инструментом .querySelector()
-const jobInput = formElement.querySelector(".popup__input_type_description"); // Воспользуйтесь инструментом .querySelector()
-const nameCardInput = formEl.querySelector(".popup__input_type_card-name"); // Воспользуйтесь инструментом .querySelector()
-const linkInput = formEl.querySelector(".popup__input_type_url"); // Воспользуйтесь инструментом .querySelector()
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
-function handleFormSubmit(evt) {
+function handleProfileFormSubmit(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   // Так мы можем определить свою логику отправки.
   // О том, как это делать, расскажем позже.
-
   // Получите значение полей jobInput и nameInput из свойства value
   let name = nameInput.value;
   let job = jobInput.value;
   // Выберите элементы, куда должны быть вставлены значения полей
-  const profileTitle = document.querySelector(".profile__title");
-  const profileDesc = document.querySelector(".profile__description");
   // Вставьте новые значения с помощью textContent
   profileTitle.textContent = name;
   profileDesc.textContent = job;
@@ -88,7 +77,7 @@ function renderCards(element) {
   cardsContainer.prepend(newCard);
 }
 // форма для сохраниения карточек
-function handFormSubmit(evt) {
+function handleCardFormSubmit(evt) {
   evt.preventDefault();
   let nameCard = nameCardInput.value;
   let cardLink = linkInput.value;
@@ -99,9 +88,8 @@ function handFormSubmit(evt) {
   // Выберите элементы, куда должны быть вставлены значения полей
   renderCards(cardItem);
   closeModal(formAddCard);
-  
 }
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener("submit", handleFormSubmit);
-formEl.addEventListener("submit", handFormSubmit);
+formProfile.addEventListener("submit", handleProfileFormSubmit);
+formCard.addEventListener("submit", handleCardFormSubmit);
